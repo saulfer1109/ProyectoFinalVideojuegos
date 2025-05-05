136183a691b048c2ddf42c0241d1ccdd4f28e9d2
Gorwast <lk23@live.com.mx>
2025-05-04 17:40:35 -0700

    Add load and save

--- scripts/Importants/DataController.gd
+++ scripts/Importants/DataController.gd
@@ -2,6 +2,9 @@ extends Node
 
 signal dataChange
 
+func _ready():
+	loadGame()
+	
 var coins: int=0:
 	get:
 		return coins
@@ -10,6 +13,7 @@ var coins: int=0:
 		if(coins>=max_coins):
 			coins=max_coins
 		dataChange.emit()
+		saveGame()
 
 var max_coins: int=99:
 	get:
@@ -17,6 +21,7 @@ var max_coins: int=99:
 	set(value):
 		max_coins=max(value,max_coins)
 		dataChange.emit();
+		saveGame()
 		
 var magic: int=0:
 	get:
@@ -26,13 +31,15 @@ var magic: int=0:
 		if(magic>=max_magic):
 			magic=max_magic
 		dataChange.emit()
+		saveGame()
 
-var max_magic: int=999:
+var max_magic: int=100:
 	get:
 		return max_magic
 	set(value):
 		max_magic=max(value,max_magic)
 		dataChange.emit();
+		saveGame()
 
 var bombs: int=0:
 	get:
@@ -42,6 +49,7 @@ var bombs: int=0:
 		if(bombs>=max_bombs):
 			bombs=max_bombs
 		dataChange.emit()
+		saveGame()
 
 var max_bombs: int=50:
 	get:
@@ -49,6 +57,7 @@ var max_bombs: int=50:
 	set(value):
 		max_bombs=max(value,max_bombs)
 		dataChange.emit();
+		saveGame()
 		
 var arrow: int=5:
 	get:
@@ -58,6 +67,7 @@ var arrow: int=5:
 		if(arrow>=max_arrow):
 			arrow=max_arrow
 		dataChange.emit()
+		saveGame()
 
 var max_arrow: int=99:
 	get:
@@ -65,8 +75,9 @@ var max_arrow: int=99:
 	set(value):
 		max_arrow=max(value,max_arrow)
 		dataChange.emit();
+		saveGame()
 		
-var health: int=1:
+var health: int=0:
 	get:
 		return health
 	set(value):
@@ -74,6 +85,7 @@ var health: int=1:
 		if(health>=max_health):
 			health=max_health
 		dataChange.emit()
+		saveGame()
 
 var max_health: int=5:
 	get:
@@ -81,3 +93,46 @@ var max_health: int=5:
 	set(value):
 		max_health=max(value,max_health)
 		dataChange.emit();
+		saveGame()
+		
+func saveGame():
+	var file = FileAccess.open("user://dataGame.json", FileAccess.WRITE)
+	
+	var data = {}
+	data["coins"] = coins
+	data["maxCoins"] = max_coins
+	data["magic"] = magic
+	data["maxMagic"] = max_magic
+	data["bombs"] = bombs
+	data["maxBombs"] = max_bombs
+	data["arrows"] = arrow
+	data["maxArrows"] = max_arrow
+	data["health"] = health
+	data["maxHealth"] = max_health
+	
+	var json = JSON.stringify(data)
+	file.store_string(json)
+	file.close()
+	
+
+func loadGame():
+	var file = FileAccess.open("user://dataGame.json", FileAccess.READ)
+	if file == null:
+		return
+	
+	var json = file.get_as_text()
+	var data = JSON.parse_string(json)
+	
+	data["coins"] = coins
+	data["maxCoins"] = max_coins
+	data["magic"] = magic
+	data["maxMagic"] = max_magic
+	data["bombs"] = bombs
+	data["maxBombs"] = max_bombs
+	data["arrows"] = arrow
+	data["maxArrows"] = max_arrow
+	data["health"] = health
+	data["maxHealth"] = max_health
+	
+	file.close()
+	
